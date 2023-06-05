afe3fa50cc49914245cb62e98d2e8f9857588881
Muffinz1 <amiryou120@gmail.com>
2023-06-05 15:31:44 -0700

    list checker

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "list.h"
+/**
+ * check_cycle - a function that checks for the cycle in the linked list
+ *
+ * @list: input list
+ * Return: 1 if there is a cycle , 0 (success)
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *clean = list;
+	listint_t *infected = list;
+
+	if (list == 0)
+		return (0);
+
+	while (clean && infected && clean->next)
+	{
+		infected = infected->next;
+		clean = clean->next->next;
+		if (clean == infected)
+			return (1);
+	}
+	return (0);
+}
