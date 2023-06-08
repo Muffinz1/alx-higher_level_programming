a6a780889adb446815c1b429079b5341009490ec
Muffinz1 <amiryou120@gmail.com>
2023-06-08 14:36:49 -0700

    Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+if __name__ == "__main__":
+    length = len(argv)
+    count = 0
+    if length == 1:
+        print("{}".format(count))
+    else:
+        for i in range(1, length):
+            count += int(argv.__getitem__(i))
+        print("{}".format(count))
