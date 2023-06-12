78b2b6f6e072f2560f0b9fa98259aaa3187af644
Muffinz1 <amiryou120@gmail.com>
2023-06-12 13:20:01 -0700

    multi returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) > 0:
-        first_char =sentence[0]
+        first_char = sentence[0]
     else:
         first_char = None
     return(len(sentence), first_char)
