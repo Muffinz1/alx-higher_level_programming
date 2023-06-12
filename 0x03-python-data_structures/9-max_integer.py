7f25fe2b6f84df3b3a657729e32d006587e669bb
Muffinz1 <amiryou120@gmail.com>
2023-06-12 13:26:37 -0700

    finding the biggest fish

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    i = my_list[0]
+    for j in my_list:
+        if j > i:
+            i = j
+    return i
+
