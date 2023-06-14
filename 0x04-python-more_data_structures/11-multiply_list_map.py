2b1b1b3bec2ceb6f5ac1008a1afdf0df967b5dd4
Muffinz1 <amiryou120@gmail.com>
2023-06-14 13:58:04 -0700

    multi list

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -2,4 +2,4 @@
 def multiply_list_map(my_list=[], number=0):
     if my_list is None:
         my_list = []
-    return list(map(lambda i: i * number, my_list))
+return list(map(lambda i: i * number, my_list))
