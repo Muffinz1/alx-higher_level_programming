0722e5e9dc6034c3ee46e77673081dd9470c15fd
Muffin <amiryou120@gmail.com>
2023-07-30 06:32:33 -0700

    a function that returns the JSON representation of an object

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+'''defines a function called to_json_string'''
+
+
+def to_json_string(my_obj):
+    ''' a function that returns a json dumb'''
+    return json.dumps(my_obj)
