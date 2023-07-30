636e3d574813a27d161afbb0a84a167f81f616e4
Muffin <amiryou120@gmail.com>
2023-07-30 12:39:07 -0700

    base geometry with area and exception raised

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a class BaseGeometry"""
+
+
+class BaseGeometry:
+    '''Public instance method: def area(self)'''
+
+    def area(self):
+        '''Exception to be raised for area()'''
+        raise Exception("area() is not implemented")
