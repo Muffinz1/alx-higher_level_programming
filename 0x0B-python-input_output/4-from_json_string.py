060a1c224734c6eed251c2615ec54d44706e1532
Muffin <amiryou120@gmail.com>
2023-07-30 10:07:09 -0700

    from json string object function

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines from_json_string function to return object"""
+import json
+
+
+def from_json_string(my_str):
+    '''scripts that return a json loads my_string'''
+    return json.loads(my_str)
