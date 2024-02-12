2d5841fd259a748f98157c98a0cf514af02d3347
muffin <muffin@muffin.com>
2024-02-12 12:11:18 -0800

    multi loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const c = 'C is fun';
+const python = 'Python is cool';
+const js = 'JavaScript is amazing';
+
+const shelf = [c, python, js];
+let i = 0;
+while (i < 3) {
+  console.log(shelf[i]);
+  i++;
+}
