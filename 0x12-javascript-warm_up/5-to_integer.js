fee41aa64d4b10be19835816bfc607a294ddbef3
muffin <muffin@muffin.com>
2024-02-12 12:04:05 -0800

    number check

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
 const arg = process.argv[2];
-const test = parseFloat(arg);
-
+const test = Math.floor(Number(arg));
 if (Number.isInteger(test)) {
-  console.log('My number:',arg);
+  console.log('My number:', arg);
 } else {
   console.log('Not a number');
 }
