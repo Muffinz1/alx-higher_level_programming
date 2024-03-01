22cfb47df8d0d91236e10eb5701839036e80edc2
muffin <muffin@muffin.com>
2024-02-29 17:15:38 -0800

    options

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # methods displayed with a certain url
-curl -s -I -X OPTIONS "$i" | grep -i 'Allow' | awk '{print $2}'
+curl -s -X OPTIONS "$i" -i | grep -i "Allow:" | awk '{print $2}'
