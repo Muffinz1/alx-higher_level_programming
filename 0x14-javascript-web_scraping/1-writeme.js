4ff725b64eaec2cffd215b74f9996a2b1359336b
muffin <muffin@muffin.com>
2024-03-27 05:11:34 -0700

    writing files

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,8 +3,8 @@
 const f = require('fs');
 const fileName = process.argv[2];
 const fileInput = process.argv[3];
-const fileContent = 'utf8'
-f.writeFile(fileName, fileInput,fileContent, function (error) {
+const fileContent = 'utf8';
+f.writeFile(fileName, fileInput, fileContent, function (error) {
   if (error) {
     console.log(error);
   }
