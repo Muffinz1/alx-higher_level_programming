b3d12677c8553056306f56f656b68cc3e80da17b
muffin <muffin@muffin.com>
2024-03-27 18:21:21 -0700

    Wedge Antilles located

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,11 +6,11 @@ let count = 0;
 request(starInput, function (_error, _respond, obj) {
   obj = JSON.parse(obj).results;
 
-  for (let x = 0; x < obj.length; ++x){
-    const chara = obj[x].chara;
+  for (let x = 0; x < obj.length; ++x) {
+    const chars = obj[x].chars;
 
-    for (let y = 0; y < chara.length; ++y){
-      const chara = chara[y];
+    for (let y = 0; y < chars.length; ++y) {
+      const chara = chars[y];
       const charaId = chara.split('/')[5];
       if (charaId === '18') {
         count = count + 1;
